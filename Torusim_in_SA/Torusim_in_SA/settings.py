37d785bb0ff7880eb15af6d720d02bf9b190310a
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-06-06 15:15:00 +0300

    main for static

--- Torusim_in_SA/Torusim_in_SA/settings.py
+++ Torusim_in_SA/Torusim_in_SA/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j0rsc@se$f6i9xi)5psviy0zyes6e&=a(c+_wqv#%ds2v8gs+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
